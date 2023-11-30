d4db509a4aa7cad807edfd7cec23a43c3e483cbe
arjjones <arjjones@kth.se>
2023-11-30 19:02:04 +0100

    Initial results summary presenter

--- src/presenter/resultsSummaryPresenter.jsx
+++ src/presenter/resultsSummaryPresenter.jsx
@@ -2,5 +2,5 @@ import ResultsSummaryView from "../view/resultsSummaryView.jsx";
 
 export default
 function ResultsSummary(){
-    return (<ResultsSummaryView />);
+    return (<ResultsSummaryView dogRecommendations={[]}/>);
 }
