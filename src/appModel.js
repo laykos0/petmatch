5af746873df726124878f45ec8d597f189d2867a
arjjones <arjjones@kth.se>
2023-11-30 17:36:06 +0100

    actually defined the model

---
+++ src/appModel.js
@@ -0,0 +1,7 @@
+import User from "./user.js"
+import Dog from "./dog.js"
+export default{
+    user: User,
+    currently_displayed_dog: Dog,
+    currently_recommended_dogs: [], // array of dogs
+}
---
+++ src/personalityAttributes.js
@@ -0,0 +1,12 @@
+export default {
+    good_with_other_dogs: 0,
+    good_with_strangers: 0,
+    playfulness: 0,
+    protectiveness: 0,
+    trainability: 0,
+    energy: 0,
+    barking: 0,
+    average_life_expectancy: 0,
+    average_weight: 0,
+    average_height: 0,
+}
