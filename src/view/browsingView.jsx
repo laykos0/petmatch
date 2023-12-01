128c4281107d8ed80356f9a0306df884591e034e
Aditya Gupta <adigup@kth.se>
2023-12-01 17:18:02 +0100

    Removed Comments

--- src/view/browsingView.jsx
+++ src/view/browsingView.jsx
@@ -5,85 +5,13 @@ import Draggable from 'react-draggable';
 import "../index.css"
 
 /* Functional JSX component. Name must start with capital letter */
-// function BrowsingView(props){
-//     // return(
-//     // <div>
-//     //     I'm the browsing view
-//     // </div>
-//     // );    
+function BrowsingView(props){
+    // return(
+    // <div>
+    //     I'm the browsing view
+    // </div>
+    // );    
 
-//     function SwipeCard({ profile, onSwipe }){
-//         const handleStop = (event, data) => {
-//             const threshold = 100;
-            
-//             if (Math.abs(data.x) > threshold) {
-//                 // Determine the direction of the swipe
-//                 const direction = data.x > 0 ? 'right' : 'left';
-//                 onSwipe(direction);
-//             }
-//         };
-        
-//         return (
-//             <Draggable
-//                 axis="x"
-//                 handle=".handle"
-//                 defaultPosition={{ x: 0, y: 0 }}
-//                 position={null}
-//                 grid={[25, 25]}
-//                 scale={1}
-//                 onStop={handleStop}
-//             >
-//                 <div className="swipe-card handle">
-//                 <div className="profile-content">
-//                     {/* Your card content goes here */}
-//                     <img src={profile.image} alt={profile.name} />
-//                     <h3>{profile.name}</h3>
-//                 </div>
-//                 </div>
-//             </Draggable>
-//         );
-//     };
-
-//     const [profiles, setProfiles] = useState([
-//         { id: 1, name: 'John Doe', image: 'john.jpg' },
-//         { id: 2, name: 'ohn oe', image: 'ohn.jpg' },
-//         { id: 3, name: 'h oe', image: 'hn.jpg' },
-//         { id: 4, name: 'n Doe', image: 'n.jpg' },
-//         // Add more profiles as needed
-//     ]);
-
-
-//     const [currentIndex, setCurrentIndex] = useState(0);
-
-//     const handleSwipe = (direction) => {
-//         // Implement logic to handle swiping effect
-//         if (direction === 'right') {
-//         console.log('Swiped right!');
-//         // Add logic for a right swipe (e.g., remove current card, add to liked profiles)
-//         } else if (direction === 'left') {
-//         console.log('Swiped left!');
-//         // Add logic for a left swipe (e.g., remove current card)
-//         }
-
-//         // Update the current index to show the next profile
-//         setCurrentIndex((prevIndex) => prevIndex + 1);
-//     };
-
-//     return (
-//         <div className="app">
-//         {profiles.map((profile, index) => (
-//             <SwipeCard
-//             key={profile.id}
-//             profile={profile}
-//             onSwipe={handleSwipe}
-//             style={{ display: index === currentIndex ? 'block' : 'none' }}
-//             />
-//         ))}
-//         </div>
-//     );
-// }
-
-function BrowsingView(props) {
     const [profiles, setProfiles] = useState([
         { id: 1, name: 'John Doe', image: 'john.jpg' },
         { id: 2, name: 'ohn oe', image: 'ohn.jpg' },
@@ -94,17 +22,14 @@ function BrowsingView(props) {
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const handleSwipe = (direction) => {
-        // Implement logic to handle swiping effect
+    function handleSwipe(direction){
         if (direction === 'right') {
             console.log('Swiped right!');
-            // Add logic for a right swipe (e.g., remove current card, add to liked profiles)
-        } else if (direction === 'left') {
+        } 
+        else if (direction === 'left') {
             console.log('Swiped left!');
-            // Add logic for a left swipe (e.g., remove current card)
         }
 
-        // Update the current index to show the next profile
         setCurrentIndex((prevIndex) => prevIndex + 1);
     };
 
@@ -113,7 +38,6 @@ function BrowsingView(props) {
             const threshold = 100;
 
             if (Math.abs(data.x) > threshold) {
-                // Determine the direction of the swipe
                 const direction = data.x > 0 ? 'right' : 'left';
                 onSwipe(direction);
             }
@@ -157,9 +81,3 @@ function BrowsingView(props) {
 }
 
 export default BrowsingView;
-
-
-
- 
-
-
