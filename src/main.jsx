fe75701da59e2c149aa87a89a2d607c55a5293a0
laykos0 <laykos0@protonmail.com>
2023-12-02 15:55:19 +0100

    Add template of authentication and include imports

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom/client'
 import ReactRoot from "./presenter/ReactRoot.jsx";
 
 import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider, onAuthStateChanged, signOut} from "firebase/auth";
 import { getDatabase } from 'firebase/database';
 import firebaseConfig from './firebaseConfig.js';
 
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db= getDatabase(app)
+const provider = new GoogleAuthProvider();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ReactRoot/>
