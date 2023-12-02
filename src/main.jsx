1525022ccea5a858b49ea1972f88942f8e94aab2
laykos0 <laykos0@protonmail.com>
2023-12-02 14:47:33 +0100

    Import firebase, firebaseConfig and initialize firebase in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import ReactRoot from "./presenter/ReactRoot.jsx";
 
+import { initializeApp } from "firebase/app";
+import firebaseConfig from '../firebaseConfig.js';
+
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ReactRoot/>
