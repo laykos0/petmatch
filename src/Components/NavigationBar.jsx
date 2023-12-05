5b5f703012241ebee9fdcb4572ad9060817ae138
Aditya Gupta <adigup@kth.se>
2023-12-05 13:51:40 +0100

    Finished rudimentary Navigation Bar
    Original approach was overcomplicated as I tried to make every part of it modular, but it was unnecessary.

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -2,35 +2,17 @@ import {React, useState } from "react";
 import './Navbar.css'; 
 
 function Navigation(){
-    const dropDownOptions = [
-        {
-            path: "/",
-            title: "Home Page"
-        },
-        {
-            path: "/browsing",
-            title: "Browsing Page"
-        },
-        {
-            path: "/result-details",
-            title: "Result Details Page"
-        },
-        {
-            path: "/results-summary",
-            title: "Results Summary Page"
-        },
-    ];
     const [isDropDownOpen, setDropDownOpen] = useState(false);
-
     return (
             <nav className="navbar">
               <div className="nav-item dropdown" onClick={()=>{setDropDownOpen(!isDropDownOpen)}}>
                 <span>Menu</span>
                 {isDropDownOpen && (
                   <div className="dropdown-menu">
-                    <a href="/result-details">Result Details Page</a>
-                    <a href="/results-summary">Results Summary Page</a>
-                    <a href="/">Welcome Page</a>
+                    <a href="#/">Welcome Page</a>
+                    <a href="#/browsing">Browsing Page</a>
+                    <a href="#/results-summary">Results Summary Page</a>
+                    <a href="#/result-details">Result Details Page</a>
                   </div>
                 )}
               </div>
