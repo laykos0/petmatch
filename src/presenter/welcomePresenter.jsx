31fbe784bc280620d5323697d204f3c70d3e47b5
laykos0 <laykos0@protonmail.com>
2023-12-06 16:47:58 +0100

    Add new presenter Welcome that handles navigation onAuthStateChanged and renders the Welcome view

--- src/presenter/welcomePresenter.jsx
+++ src/presenter/welcomePresenter.jsx
@@ -1,6 +1,35 @@
+import React, { useEffect, useRef } from "react";
+import auth from "../services/auth.js";
 import WelcomeView from "../view/welcomeView.jsx";
+import { useNavigate } from "react-router-dom"
 
-export default
-function Welcome(){
-    return (<WelcomeView />);
+export default function Welcome() {
+  
+  const navigate = useNavigate();
+  const unsubscribeRef = useRef(null);
+
+  useEffect(() => {
+    unsubscribeRef.current = auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigate("/browsing");
+      } else {
+        navigate("/")
+      }
+    });
+  }, [navigate]);
+
+
+  function onSignInEmail(email, password) {
+    auth.signInEmailPassword(email, password);
+  }
+
+  function onSignInGoogle() {
+    auth.signInGoogle();
+  }
+
+  function onSignUpEmail(email, password) {
+    auth.signUpEmailPassword(email, password);
+  }
+
+  return <WelcomeView onSignInEmail={onSignInEmail} onSignInGoogle={onSignInGoogle} onSignUpEmail={onSignUpEmail} />;
 }
