6e479d356cef96bc5c5e2dd61750a73210613ff3
laykos0 <laykos0@protonmail.com>
2023-12-06 16:46:23 +0100

    Add singupView.jsx

--- src/view/signupView.jsx
+++ src/view/signupView.jsx
@@ -1,75 +1,72 @@
-/* Functional JSX component. Name must start with capital letter */
-import '../SignupForm.css';
 import React, { useState } from 'react';
 
-function SignupView(props){
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-    // function handleNameChange(event) {
-    //   setName(event.target.value);
-    // }
-  
-    function handleEmailChange(event) {
-      setEmail(event.target.value);
-    }
-  
-    function handlePasswordChange(event) {
-      setPassword(event.target.value);
-    }
-  
-    function handleSignInEmailPassword() {
-      props.onSignInEmail(email, password)
-    }
+function SignupView(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-    function handleSignInGoogle() {
-      props.onSignInGoogle()
-    }
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
 
-    function handleSignUp() {
-      props.onSignUpEmail(email, password)
-    }
-  
-    return (
-      <div className="signup-form">
-        <h2>Sign Up</h2>
-        {/* <div className="form-group">
-          <label htmlFor="name" className="form-label">Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={handleNameChange}
-            className="form-input"
-          />
-        </div> */}
-        <div className="form-group">
-          <label htmlFor="email" className="form-label">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-            className="form-input"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password" className="form-label">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            className="form-input"
-          />
-        </div>
-        <button onClick={handleSignInEmailPassword} className="sign-in-btn">Sign In</button>
-        <button onClick={handleSignInGoogle} className="sign-in-btn">Sign In Google</button>
-        <button onClick={handleSignUp} className="sign-up-btn">Sign Up</button>
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleConfirmPasswordChange(event) {
+    setConfirmPassword(event.target.value);
+  }
+
+  function handleSignInGoogle() {
+    props.onSignInGoogle();
+  }
+
+  function handleSignUp() {
+    props.onSignUpEmail(email, password);
+  }
+
+  function handleToggleView() {
+    props.onToggleView();
+  }
+
+  return (
+    <div className="signup-form">
+      <h2>Sign Up</h2>
+      <div className="form-group">
+        <label htmlFor="email" className="form-label">
+          Email:
+        </label>
+        <input type="email" id="email" value={email} onChange={handleEmailChange} className="form-input" />
       </div>
-    );
-
+      <div className="form-group">
+        <label htmlFor="password" className="form-label">
+          Password:
+        </label>
+        <input type="password" id="password" value={password} onChange={handlePasswordChange} className="form-input" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="confirmPassword" className="form-label">
+          Confirm Password:
+        </label>
+        <input
+          type="password"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+          className="form-input"
+        />
+      </div>
+      <button onClick={handleSignUp} className="sign-up-btn">
+        Sign Up
+      </button> 
+      <button onClick={handleSignInGoogle} className="sign-in-btn">
+        Sign Up Google
+      </button>
+      <span style={{ textDecoration: 'underline', cursor: 'pointer', color: 'blue' }} onClick={handleToggleView}>
+        {'Already have an account? Log in'}
+        </span>
+    </div>
+  );
 }
 
 export default SignupView;
