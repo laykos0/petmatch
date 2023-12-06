fb4b40f528acf86ba7bb2cf3c031752215145007
laykos0 <laykos0@protonmail.com>
2023-12-06 15:29:14 +0100

    Implement signupPresenter.jsx to include methods responsible for interaction with user authentication

--- src/presenter/signupPresenter.jsx
+++ src/presenter/signupPresenter.jsx
@@ -1,6 +1,26 @@
+import auth from "../services/auth.js"
 import SignupView from "../view/signupView.jsx";
 
 export default
 function Signup(){
-    return (<div><SignupView /> </div>);
+
+    function onSignInEmailACB(email, password) {
+        auth.signInEmailPassword(email, password)
+    }
+
+    function onSignInGoogleACB() {
+        auth.signInGoogle()
+    }
+
+    function onSignUpEmailACB(email, password) {
+        auth.signUpEmailPassword(email, password)
+    }
+
+    return (<div>
+                <SignupView 
+                    onSignInEmail={onSignInEmailACB}
+                    onSignInGoogle={onSignInGoogleACB}
+                    onSignUpEmail={onSignUpEmailACB}
+                /> 
+            </div>);
 }
