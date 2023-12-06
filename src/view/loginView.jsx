fd0ba7100154afd5642719c4cefb8709200fbd7a
laykos0 <laykos0@protonmail.com>
2023-12-06 16:46:13 +0100

    Add LoginView in loginView.jsx

---
+++ src/view/loginView.jsx
@@ -0,0 +1,56 @@
+// LoginView.jsx
+import React, { useState } from 'react';
+
+function LoginView(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSignInEmailPassword() {
+    props.onSignInEmail(email, password);
+  }
+
+  function handleSignInGoogle() {
+    props.onSignInGoogle();
+  }
+
+  function handleToggleView() {
+    props.onToggleView();
+  }
+
+  return (
+    <div className="login-form">
+      <h2>Login</h2>
+      <div className="form-group">
+        <label htmlFor="email" className="form-label">
+          Email:
+        </label>
+        <input type="email" id="email" value={email} onChange={handleEmailChange} className="form-input" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password" className="form-label">
+          Password:
+        </label>
+        <input type="password" id="password" value={password} onChange={handlePasswordChange} className="form-input" />
+      </div>
+      <button onClick={handleSignInEmailPassword} className="sign-in-btn">
+        Sign In
+      </button>
+      <button onClick={handleSignInGoogle} className="sign-in-btn">
+        Sign In Google
+      </button>
+      <span style={{ textDecoration: 'underline', cursor: 'pointer', color: 'blue' }} onClick={handleToggleView}>
+        {"Don't have an account? Click here"}
+      </span>
+    </div>
+  );
+}
+
+export default LoginView;
