f4c2032229abedc76910fa77e688be17c0ab5fb2
laykos0 <laykos0@protonmail.com>
2023-12-06 18:14:23 +0100

    Correct missing main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,6 @@
 import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ReactRoot from "./presenter/ReactRoot.jsx";
-import NavBar from './Components/NavigationBar';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './presenter/app.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <div>
-        <NavBar/>
-        <ReactRoot/>
-    </div>
-)
-
-// import auth from "./services/auth.js"
-// window.auth = auth
-// import userModel from "./model/user.js"
-// window.model = userModel
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
