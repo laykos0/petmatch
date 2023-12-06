594dd3a78ad149398a4b06dc33b32423a2bc6c64
laykos0 <laykos0@protonmail.com>
2023-12-06 02:26:00 +0100

    Remove app, auth, db and provider initialization from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ReactRoot from "./presenter/ReactRoot.jsx";
 
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getDatabase } from 'firebase/database';
-import firebaseConfig from './firebaseConfig.js';
 
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db= getDatabase(app)
-const provider = new GoogleAuthProvider();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ReactRoot/>
-)
+);
+import userModel from "./model/user.js"
+window.model = userModel
