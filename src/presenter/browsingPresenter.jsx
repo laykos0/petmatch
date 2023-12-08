e326bc404a380ca00b293e693a6205bb01d15d54
laykos0 <laykos0@protonmail.com>
2023-12-08 20:08:42 +0100

    Modify Browsing presenter to conditionally load the displayed dog, otherwise render loading image. Add sleep() function for testing. Use useEffect and useState to implement suspense when waiting for API response. Implement rateACB, callback used to update user preferences and generate new dog to display

--- src/presenter/browsingPresenter.jsx
+++ src/presenter/browsingPresenter.jsx
@@ -1,20 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import BrowsingView from "../view/browsingView.jsx";
 
-export default
-function Browsing(props){
-
-    function xACB(likeOrDislike) {
-        props.model.generateDisplayDog()
-        //console.log(props.model.currentlyDisplayedDog.name)
-        if (likeOrDislike) {
-            // Handle Like
-        } else {
-            // Handle Dislike
+export default function Browsing(props){
+    const [isLoading, setIsLoading] = useState(true);
+    const modelToPass = isLoading ? {
+        currentlyDisplayedDog: {
+            name: "Loading",
+            image_link: "https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca.gif"
         }
+    } : props.model;
+
+    useEffect(() => {
+        async function fetchData() {
+            await props.model.generateDisplayDog();
+            setIsLoading(false);
+        }
+    
+        fetchData();
+    }, []); 
+
+    async function rateACB(like) {
+        props?.model?.user?.updateUserInformation(props.model.currentlyDisplayedDog, like)
+        setIsLoading(true); 
+        props?.model?.generateDisplayDog()
+        await sleep(1000); 
+        setIsLoading(false); 
     }
 
-    props.model.generateDisplayDog()
-
-    console.log(props.model.currentlyDisplayedDog.name)
-    return (<BrowsingView model={props.model} x={xACB} />);
+    return (
+        <div>
+            <BrowsingView model={modelToPass} rate={rateACB} />
+        </div>
+    );
 }
+
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
