b636a36a642f7005695196bab65fd60b8b31fea1
laykos0 <laykos0@protonmail.com>
2023-12-08 14:13:35 +0100

    Modify Browsing presenter to include callback that generates dog to display, pass props to BrowsingView

--- src/presenter/browsingPresenter.jsx
+++ src/presenter/browsingPresenter.jsx
@@ -1,6 +1,10 @@
 import BrowsingView from "../view/browsingView.jsx";
 
 export default
-function Browsing(){
-    return (<BrowsingView />);
+function Browsing(props){
+    function generateDogACB() {
+        props.model.generateDisplayDog()
+    }
+
+    return (<BrowsingView model={props.model} generateDog={generateDogACB} />);
 }
