54163fd97aa67b2a4656588a16a9213654feab59
Aditya Gupta <adigup@kth.se>
2023-12-08 23:16:24 +0100

    added tailwind directives and updates css files.

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -6,9 +6,13 @@ function Navbar(){
     return (
             <nav className="navbar">
               <div className="nav-item dropdown" onClick={()=>{setDropDownOpen(!isDropDownOpen)}}>
-                <span><button>Menu</button></span>
+                <span>
+                  <button className="text-xl font-bold text-purple-300 w-20 rounded hover:rounded-lg ">
+                    Menu
+                  </button>
+                </span>
                 {isDropDownOpen && (
-                  <div className="dropdown-menu">
+                  <div className="dropdown-menu font-sans text-purple-200">
                     <a href="#/">Welcome Page</a>
                     <a href="#/browsing">Browsing Page</a>
                     <a href="#/results-summary">Results Summary Page</a>
