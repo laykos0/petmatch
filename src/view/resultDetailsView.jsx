e475d7715d5b058498e04bb366544f38278848b1
15ajones <15ajones0201@gmail.com>
2023-12-08 17:46:48 +0100

    made nicer

--- src/view/resultDetailsView.jsx
+++ src/view/resultDetailsView.jsx
@@ -1,25 +1,9 @@
 /* Functional JSX component. Name must start with capital letter */
 function ResultDetailsView(props){
     
-    function renderDog(){
-        if (props.currentlyDisplayedDog){
-            return (<div>{props.currentlyDisplayedDog.name} </div>);
-        } else {
-            return <div> loading</div>;
-        }
-    }
-    
     return(
     <div>
-        I'm the result details view. Number is:
-        {props.numberToDisplay}
-        after number
-        the name of the user is:
-        {props.model.user.name}
-        <button onClick={() => {props.click()}}>click me</button>
-        displaying dogs:
-        {renderDog()}
-        
+        I'm the result details view. I'm a work in progress!
     </div>
     
     );
