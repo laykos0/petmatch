e8bf0d6ac9480f636f241c228e8fee27649cd6dd
Aditya Gupta <adigup@kth.se>
2023-12-09 20:57:34 +0100

    manually added some of the files from main.

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,22 +1,21 @@
 import {React, useState } from "react";
 import '../styles/navbar.css'; 
+import auth from "../services/auth"
+
 
 function Navbar(){
     const [isDropDownOpen, setDropDownOpen] = useState(false);
     return (
             <nav className="navbar">
               <div className="nav-item dropdown" onClick={()=>{setDropDownOpen(!isDropDownOpen)}}>
-                <span>
-                  <button className="text-xl font-bold text-purple-300 w-20 rounded hover:rounded-lg ">
-                    Menu
-                  </button>
-                </span>
+                <span><button>Menu</button></span>
                 {isDropDownOpen && (
-                  <div className="dropdown-menu font-sans text-purple-200">
+                  <div className="dropdown-menu">
                     <a href="#/">Welcome Page</a>
                     <a href="#/browsing">Browsing Page</a>
                     <a href="#/results-summary">Results Summary Page</a>
                     <a href="#/result-details">Result Details Page</a>
+                    <a onClick={() => auth.signOut()}>Log Out</a>
                   </div>
                 )}
               </div>
