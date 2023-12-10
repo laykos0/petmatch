cd368d767be9f721d9d690c2b1e2a390699226ad
Aditya Gupta <adigup@kth.se>
2023-12-10 15:29:06 +0100

    Navbar tailwind
    Added subtle gradient to navbar and dropdown and shortened dropdown link names

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -8,13 +8,17 @@ function Navbar(){
     return (
             <nav className="navbar">
               <div className="nav-item dropdown" onClick={()=>{setDropDownOpen(!isDropDownOpen)}}>
-                <span><button>Menu</button></span>
+                <span>
+                  <button className="text-xl font-bold text-purple-300 w-20 rounded hover:rounded-lg">
+                      Menu
+                  </button>
+                </span>
                 {isDropDownOpen && (
-                  <div className="dropdown-menu">
-                    <a href="#/">Welcome Page</a>
-                    <a href="#/browsing">Browsing Page</a>
-                    <a href="#/results-summary">Results Summary Page</a>
-                    <a href="#/result-details">Result Details Page</a>
+                  <div className="dropdown-menu font-sans text-purple-200">
+                    <a href="#/">Home</a>
+                    <a href="#/browsing">Browse</a>
+                    <a href="#/results-summary">Results</a>
+                    <a href="#/result-details">Details</a>
                     <a onClick={() => auth.signOut()}>Log Out</a>
                   </div>
                 )}
