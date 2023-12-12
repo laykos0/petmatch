fb76e80362936b775b5f843bb0ff313be3f9b337
Aditya Gupta <adigup@kth.se>
2023-12-12 22:21:13 +0100

    Redid the CSS for the About Page
    1. Added some basic stuff to the "Who are we?" section.
    2. Aligned the images with the method and insight sections on the about page.
    3. Renamed purple-box to just box.
    4. Changed Text scaling for the different sections.
    5. Changed Text color.

--- src/view/aboutView.jsx
+++ src/view/aboutView.jsx
@@ -1,52 +1,37 @@
 import "../styles/about.css";
-import UserEvalImage1 from '../images/LoFi_User_Eval.jpg'; // Image for Formative Evaluation
-import UserEvalImage2 from '../images/Pointing_Image_Lofi_Eval.jpg'; // Image for Summative Evaluation
+import UserEvalImage1 from '../images/LoFi_User_Eval.jpg';
+import UserEvalImage2 from '../images/Pointing_Image_Lofi_Eval.jpg';
 
 function AboutView() {
   return (
-    <div className="page-container">
+    <div className="page-container my-10">
       <div className="section">
         <h2 className="section-title">Who are we?</h2>
-        <p>Add your content about who you are here...</p>
+        <p>
+          We are a group of students working together to make a project for the course DH2642 (Interaction Programming and Dynamic Web) at KTH Royal Insititute of Technology. The focus of the course is on User Centric Design.  
+        </p>
+        <br />
+        <div className=" text-center font-bold">
+        <div className=" text-2xl"> Group Members: </div>
+        <ul>
+          <li>Aditya Gupta</li>
+          <li>Anthony Jones</li>
+          <li>Jakub Rybak</li>
+          <li>Maia Ramambason</li>
+        </ul>
+        </div>
       </div>
 
       <div className="section">
         <h2 className="section-title">User Evaluation</h2>
-
-        {/* Formative Evaluation Section */}
         <div className="evaluation-section">
-          <h3 className="subsection-title">Formative Evaluation</h3>
-          <div className="evaluation-content">
-            <div className="sub-section purple-box">
-              <h4 className="sub-section-title">Method</h4>
-              <ul>
-                <li>Point one about the method...</li>
-                <li>Point two about the method...</li>
-                <li>Point three about the method...</li>
-              </ul>
-            </div>
-            <div className="sub-section purple-box">
-              <h4 className="sub-section-title">Insights</h4>
-              <ul>
-                <li>Insight one...</li>
-                <li>Insight two...</li>
-                <li>Insight three...</li>
-              </ul>
-            </div>
+          <div className="evaluation-content row-container">
             <div className="sub-section">
-              <h4 className="sub-section-title">Images</h4>
               <div className="user-evaluation-images">
                 <img src={UserEvalImage1} alt="User Evaluation Image 1" />
               </div>
             </div>
-          </div>
-        </div>
-
-        {/* Summative Evaluation Section */}
-        <div className="evaluation-section">
-          <h3 className="subsection-title">Summative Evaluation</h3>
-          <div className="evaluation-content">
-            <div className="sub-section purple-box">
+            <div className="sub-section box">
               <h4 className="sub-section-title">Method</h4>
               <ul>
                 <li>Point one about the method...</li>
@@ -54,7 +39,7 @@ function AboutView() {
                 <li>Point three about the method...</li>
               </ul>
             </div>
-            <div className="sub-section purple-box">
+            <div className="sub-section box">
               <h4 className="sub-section-title">Insights</h4>
               <ul>
                 <li>Insight one...</li>
@@ -62,11 +47,32 @@ function AboutView() {
                 <li>Insight three...</li>
               </ul>
             </div>
+          </div>
+        </div>
+
+        <div className="evaluation-section">
+          <h3 className="section-title">Summative Evaluation</h3>
+          <div className="evaluation-content row-container">
             <div className="sub-section">
-              <h4 className="sub-section-title">Images</h4>
-              <div className="user-evaluation-images">
-                <img src={UserEvalImage2} alt="User Evaluation Image 2" />
-              </div>
+                <div className="user-evaluation-images">
+                  <img src={UserEvalImage2} alt="User Evaluation Image 2" />
+                </div>
+            </div>
+            <div className="sub-section box">
+              <h4 className="sub-section-title">Method</h4>
+              <ul>
+                <li>Point one about the method...</li>
+                <li>Point two about the method...</li>
+                <li>Point three about the method...</li>
+              </ul>
+            </div>
+            <div className="sub-section box">
+              <h4 className="sub-section-title">Insights</h4>
+              <ul>
+                <li>Insight one...</li>
+                <li>Insight two...</li>
+                <li>Insight three...</li>
+              </ul>
             </div>
           </div>
         </div>
