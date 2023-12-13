0987920f864014e547439eaed7d9399fc3ff1a08
15ajones <15ajones0201@gmail.com>
2023-12-13 19:57:48 +0100

    Can now update user locatioon

--- src/presenter/profilePresenter.jsx
+++ src/presenter/profilePresenter.jsx
@@ -1,5 +1,25 @@
 import ProfileView from "../view/profileView.jsx";
+import { observer } from "mobx-react-lite";
 
-export default function Profile() {
-  return <ProfileView onSignInEmail={()=>{console.log("hi1")}} onSignInGoogle={()=>{console.log("hi2")}}/>;
+export default observer(
+function Profile(props) {
+  function updateLocationCustomACB(zipCode, stateCode){
+    function checkValidZipCode(zipCode){
+      return /^\d{5}(-\d{4})?$/.test(zipCode);
+    }
+    function checkValidStateCode(stateCode){
+      return /^[a-zA-Z]{2}$/.test(stateCode);
+    }
+    if(!checkValidZipCode(zipCode)){
+      alert("Invalid Zip Code! Please try again");
+      return;
+    }
+    if(!checkValidStateCode(stateCode)){
+      alert("Invalid State Code! Please try again!");
+      return;
+    }
+    props.model.updateUserLocation(zipCode, stateCode);
+  }
+  return <ProfileView updateLocation={(zipCode, stateCode)=>{updateLocationCustomACB(zipCode, stateCode)}}/>;
 }
+)
