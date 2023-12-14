287f677860ac30e6f48e5c946c1e5e8de106f668
Aditya Gupta <adigup@kth.se>
2023-12-14 22:34:50 +0100

    Finished CSS for Details View

--- src/view/resultDetailsView.jsx
+++ src/view/resultDetailsView.jsx
@@ -4,22 +4,22 @@ import '../styles/details.css';
 
 function ResultDetailsView(props) {
   return useObserver(() => (
-    <div>
+    <div className='flex'>
       {props.displayedDog.name ? (
         <div className='details-container'>
             <div>
-            <div className=' text-5xl text-center bg-orange-300 w-2/3 rounded-full content-center'> {props.displayedDog.name} </div>
-            <div className="dog-details-container">
+            <div className=' text-5xl font-extrabold bg-orange-400 rounded-full w-2/3 mx-auto text-center mt-10'> {props.displayedDog.name} </div>
+            <div className="dog-details-container flex">
                 <div className="dog-image">
                 <img src={props.displayedDog.image_link} height="100" alt={props.displayedDog.name} />
                 </div>
                 <div className="dog-details-info relative">
                     <div className='personality-attributes'>{renderPersonalityAttributes(props.displayedDog.personalityPreferences)}</div>
                         <a href="#/results-summary" className='flex justify-between'>
-                            <button onClick={removeDogACB} className="remove-button px-4 py-2 ">
-                            x
+                            <button onClick={removeDogACB} className="remove-button px-4 py-2 mt-32">
+                            Remove
                             </button>
-                            <button className="back-button px-4 py-2" onClick={() => console.log('going back')}>
+                            <button onClick={() => console.log('going back')} className="back-button px-4 py-2 mt-32">
                             Go back!
                             </button>
                         </a>
@@ -28,41 +28,44 @@ function ResultDetailsView(props) {
             </div>
 
             <div className='mt-5 mb-5 bg-white info'>
-                <div className=' bg-indigo-500 outlineText text-4xl text-slate-900'>About the Dog:</div>
+                <div className=' bg-indigo-200 font-thin text-4xl text-slate-900 text-center'>About the Dog</div>
                 <div className=' text-violet-950'>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eos, eveniet fugiat commodi corporis ipsa modi dolore rem doloremque obcaecati harum voluptates fugit sapiente nihil aliquam ex quidem praesentium quas ab voluptatibus iusto. Debitis enim expedita autem ut magni, sint dolor eos accusantium atque? Temporibus eos quos distinctio porro culpa rerum soluta possimus nam ipsam! Ducimus possimus temporibus deserunt omnis consectetur libero facere rem voluptatibus voluptates aperiam, animi dicta, eveniet exercitationem natus nesciunt tenetur impedit inventore perspiciatis accusamus dolores maxime asperiores atque odit? Dolores aut libero hic dolorum similique accusantium architecto voluptates possimus distinctio delectus sint, itaque aliquam. Cupiditate, illum.
                 {/* {props.dogDescription || <img src="https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca.gif"/>} */}
                 </div> 
             </div>
 
-            <div>
-            <div className=' text-5xl text-center bg-orange-300 w-4/5 rounded-full content-center'> Adoption shelters near you:</div>
-            </div>
-            <div>
-            <table className='rounded-lg w-4/5'>
-                <thead>
-                <tr>
-                    <th className=' bg-slate-600'>Organization Name</th>
-                    <th className=' bg-slate-600'>Distance From You</th>
-                    <th className=' bg-slate-600'>Phone Number</th>
-                    <th className=' bg-slate-600'>Website Link</th>
-                </tr>
-                </thead>
-
-                <tbody>
-                {props.organizations
-                    ? props.organizations.map(renderOrganization)
-                    : (
-                    <tr key={11}>
-                        <td />
-                        <td>loading </td>
-                        <td>loading</td>
-                        <td>loading</td>
+            <div className='bg-gray-300 rounded-lg mb-20 flex flex-col h-full rounded-lg'>
+              <div className='bg-indigo-200 font-thin text-4xl text-slate-900 text-center p-4'>
+                Adoption shelters near you
+              </div>
+              <div className='flex-1 overflow-auto'>
+                <table className='rounded w-full text-slate-900 bg-orange-300'>
+                  <thead>
+                    <tr>
+                      <th className='bg-violet-300'>Organization Name</th>
+                      <th className='bg-violet-300'>Distance From You</th>
+                      <th className='bg-violet-300'>Phone Number</th>
+                      <th className='bg-violet-300'>Website Link</th>
                     </tr>
-                    )}
-                </tbody>
-            </table>
+                  </thead>
+
+                  <tbody>
+                    {props.organizations
+                      ? props.organizations.map(renderOrganization)
+                      : (
+                        <tr key={11}>
+                          <td />
+                          <td>loading </td>
+                          <td>loading</td>
+                          <td>loading</td>
+                        </tr>
+                      )}
+                  </tbody>
+                </table>
+              </div>
             </div>
+
         </div>
       ) : (
         <h1>Please select a dog!</h1>
