1997360eb4652ad5376cfe97cbbd744e4e26b64a
Aditya Gupta <adigup@kth.se>
2023-12-14 16:22:19 +0100

    Fixed the profile CSS

--- src/view/profileView.jsx
+++ src/view/profileView.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { useObserver } from "mobx-react-lite";
-import "../styles/login.css";
+import { useObserver } from 'mobx-react-lite';
+import '../styles/profile.css';
 
 function ProfileView(props) {
   const [zipCode, setZipCode] = useState('');
@@ -19,30 +19,29 @@ function ProfileView(props) {
   }
 
   return useObserver(() => (
-    <div className="login-form my-20">
-      <h2 className='text-2xl font-thin py-5'>Update Your Location</h2>
-      <div className="form-group">
-        <label htmlFor="zipCode" className="form-label">
-          Zip Code
-        </label>
-        <input type="zipCode" id="zipCode" value={zipCode} onChange={handleZipCodeChange} className="form-input" />
-      </div>
+    <div className="profile-container">
+      <div className="profile-form">
+        <h2 className="text-3xl font-thin py-5">Update Your Location</h2>
+        <div className="form-group">
+          <label htmlFor="zipCode" className="form-label">
+            Zip Code
+          </label>
+          <input type="zipCode" id="zipCode" value={zipCode} onChange={handleZipCodeChange} className="form-input" />
+        </div>
 
-      <div className="form-group">
-        <label htmlFor="stateCode" className="form-label">
-          State Code (e.g 'MA')
-        </label>
-        <input type="stateCode" id="stateCode" value={stateCode} onChange={handleStateCodeChange} className="form-input" />
-      </div>
+        <div className="form-group">
+          <label htmlFor="stateCode" className="form-label">
+            State Code (e.g 'MA')
+          </label>
+          <input type="stateCode" id="stateCode" value={stateCode} onChange={handleStateCodeChange} className="form-input" />
+        </div>
 
-      <button onClick={handleUpdateLocation} className="sign-in-btn">
-        Update Location
-      </button>
-     
+        <button onClick={handleUpdateLocation} className="update-btn">
+          Update Location
+        </button>
+      </div>
     </div>
   ));
 }
-
-
 
 export default ProfileView;
