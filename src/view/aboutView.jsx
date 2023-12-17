2089ff0b5b9d0ec996bf45e39b90c3e8a0d637af
Aditya Gupta <adigup@kth.se>
2023-12-17 22:36:58 +0000

    Finalized about page

--- src/view/aboutView.jsx
+++ src/view/aboutView.jsx
@@ -1,63 +1,74 @@
-import UserEvalImage1 from '../images/LoFi_User_Eval.jpg';
-import UserEvalImage2 from '../images/Pointing_Image_Lofi_Eval.jpg';
+import BrowsingImage from '../images/browsing1.png';
+import ResultsImage from '../images/results1.png';
+import DetailsImage from '../images/details1.png';
 import "../styles/about.css";
-import Auth from "../services/auth"
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function AboutView() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const unsubscribe = Auth.onAuthStateChanged((user) => {
-      setIsLoggedIn(!!user);
-    });
-    return () => unsubscribe();
-  }, []);
+function AboutView(props) {
 
   return (
     <div className="page-container my-10">
       <div className="section">
-        <h2 className="section-title">What is Woof Finder?</h2>
+        <h2 className="section-title text-center text-5xl mb-10">What is Woof Finder?</h2>
         <p>
           Woof Finder is an App dedicated to helping you find a dog that will be your perfect match! Simply create an account and find your dream dog! Our goal is to match you with dogs most compatible with the personality and location you prefer. Our team, Jakub, Aditya, Anthony and Maia thought, what better way to do this than the same way we all look for love - by swiping! And most importantly, since the dogs we match you with are all shelter dogs, you will be able to give your perfect dog its forever home.
         </p>
       </div>
 
-      ({isLoggedIn &&<div className="section">
-        <h2 className="section-title">Evaluation</h2>
-        <div className="sub-section w-4/5 mx-auto">
-            <div className="user-evaluation-images flex justify-center">
-              <div className="image-grid">
-                <img src={UserEvalImage1} alt="User Evaluation Image 1" />
-                <img src={UserEvalImage2} alt="User Evaluation Image 2" />
+      <div className="section">
+        <h2 className="section-title text-center text-5xl mb-10">How does Woof Finder work?</h2>
+        <div className="sub-section mx-auto flex">
+          <div className="user-evaluation-images md:w-2/5 md:h-2/5 rounded-lg mb-10">
+            <img src={BrowsingImage} alt="Browsing Image" />
+          </div>
+          <div className="evaluation-section flex-1 px-10">
+            <div className="evaluation-content row-container">
+              <div className="sub-section bg-indigo-100 rounded-lg m-3">
+                <h4 className="sub-section-title">The Browsing Page</h4>
+                <p className='text-lg text-black p-10'>
+                  This page shows a set of different dogs, which the user can swipe through or use the like/dislike button on, in order to tailor their personalised recommendation algorithm for the type of dog they might like based on the first impression of the user upon looking at the dogs. After they have swiped through a few dogs, the user can go to the Results page to see their recommended dogs. The user can do this by clicking on the Dropdown Menu on the navigation bar.
+                </p>
               </div>
             </div>
+          </div>
         </div>
-        <div className="evaluation-section">
-          <div className="evaluation-content row-container">
-            <div className="sub-section">
-              <h4 className="sub-section-title">Formative Evaluation</h4>
-              <p>
-                Here, you can provide your evaluation text that accompanies the images.
-                <br />
-                This content will appear alongside the images.
-              </p>
+
+        <br />
+
+        <div className="sub-section mx-auto flex">
+          <div className="user-evaluation-images md:w-2/5 md:h-2/5 rounded-lg mb-10">
+            <img src={ResultsImage} alt="Results Image" />
+          </div>
+          <div className="evaluation-section flex-1 px-10">
+            <div className="evaluation-content row-container">
+              <div className="sub-section bg-indigo-100 rounded-lg m-3">
+                <h4 className="sub-section-title">The Results Page</h4>
+                <p className='text-lg text-black p-10'>
+                  This page shows a collection of dogs, based on the user's swiping actions on the browsing page and the current location set by the user on their profile. The user can click on the dog's name to get more information on the dog and in effect navigate to the details page. The recommendations are such that the personality attributes match close to the attributes of the dogs which the user liked on the browsing page. 
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="evaluation-section">
-          <div className="evaluation-content row-container">
-            <div className="sub-section">
-              <h4 className="sub-section-title">Summative Evaluation</h4>
-              <p>
-                Here, you can provide your evaluation text that accompanies the images.
-                <br />
-                This content will appear alongside the images.
-              </p>
+
+        <br />
+
+        <div className="sub-section mx-auto flex">
+          <div className="user-evaluation-images md:w-2/5 md:h-2/5 rounded-lg mb-10">
+            <img src={DetailsImage} alt="Details Image" />
+          </div>
+          <div className="evaluation-section flex-1 px-10">
+            <div className="evaluation-content row-container">
+              <div className="sub-section bg-indigo-100 rounded-lg m-3">
+                <h4 className="sub-section-title">The Details Page</h4>
+                <p className='text-lg text-black p-10'>
+                  This page is reached by clicking on the name of a dog and presents the user with information about the dog's different personality attributes and a short description of the selected dog. The user can also see a list of nearby adoption centres with their contact details at the bottom of this page. This page also allows the user to remove the dog from the recommendations if they feel that the dog doesn't match their needs or navigate back to the results page in order to browse through their other recommendations.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>})
+      </div>
 
       <div className="section contact-detailss">
         <h2 className="section-title">Contact us</h2>
