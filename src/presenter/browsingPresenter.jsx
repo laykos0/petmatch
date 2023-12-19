4519e5049f1e5e6bda7304ccc5514d63b3f0be12
Aditya Gupta <adigup@kth.se>
2023-12-19 14:06:25 +0000

    Some small changes to results CSS.

--- src/presenter/browsingPresenter.jsx
+++ src/presenter/browsingPresenter.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { React, useState, useEffect } from 'react';
 import BrowsingView from "../view/browsingView.jsx";
 import { observer } from "mobx-react-lite";
 
