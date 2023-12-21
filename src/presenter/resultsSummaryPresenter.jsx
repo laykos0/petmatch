25be5ffd87f0a5901e69be2c195f03e1aa36da57
souramam1 <maia.ramambason20@imperial.ac.uk>
2023-12-21 22:33:49 +0100

    summary presenter usestate changes

--- src/presenter/resultsSummaryPresenter.jsx
+++ src/presenter/resultsSummaryPresenter.jsx
@@ -1,7 +1,24 @@
 import ResultsSummaryView from "../view/resultsSummaryView.jsx";
 import { observer } from "mobx-react-lite";
+import { React, useState, useEffect } from 'react';
 
 export default observer(function ResultsSummary(props){
+
+    const [isLoading, setIsLoading] = useState(true);
+    const modelToPass = isLoading ? {
+        currentlyDisplayedDog: {
+            name: "Loading",
+            image_link: "https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca.gif"
+        }
+    } : props.model;
+
+    // useEffect(() => {
+    // async () => {
+    //     if (props.model.currentlyRecommendedDogs.length == 0) {
+    //         await props.model.getDogRecommendations();
+    //     }
+    // }    
+    // }, []); 
 
     function removeDogCustomACB(dog) {
         props.model.removeDogFromRecommendations(dog);
@@ -15,5 +32,9 @@ export default observer(function ResultsSummary(props){
         props.model.getDogRecommendations();
     }
 
-    return (<ResultsSummaryView selectDogCustomACB={selectDogCustomACB} dogRecommendations={props.model.currentlyRecommendedDogs} removeDogCustomACB={removeDogCustomACB}/>);
+    return (
+        <div>
+            <ResultsSummaryView model={modelToPass} selectDogCustomACB={selectDogCustomACB} dogRecommendations={props.model.currentlyRecommendedDogs} removeDogCustomACB={removeDogCustomACB}/>
+        </div>
+    );
 })
